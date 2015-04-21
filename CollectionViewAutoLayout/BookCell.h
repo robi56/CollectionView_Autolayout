54c1b1a6e8733901a4e910722cc83bb82a891650
robi <nandi@ipvision-inc.com>
2015-04-21 15:52:04 +0600

    dads

--- CollectionViewAutoLayout/BookCell.h
+++ CollectionViewAutoLayout/BookCell.h
@@ -17,5 +17,5 @@
 @property (nonatomic,retain) IBOutlet UILabel *bookTitle;
 @property (nonatomic,strong) IBOutlet UITextView *bookDescription;
 @property (nonatomic,strong) IBOutlet UITextView *dummyDescription;
-
+@property (nonatomic,strong) IBOutlet UITextView *dummydummyDescription;
 @end
