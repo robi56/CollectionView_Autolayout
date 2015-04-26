37e25d227e06aea966288cdecd49397e241e2457
robi <nandi@ipvision-inc.com>
2015-04-26 11:19:20 +0600

    sdfs

---
+++ CollectionViewAutoLayout/TableViewViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewViewController.h
+//  CollectionViewAutoLayout
+//
+//  Created by Mac-8 on 4/26/15.
+//  Copyright (c) 2015 IPvision Canada Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewViewController : UICollectionViewController
+
+@end
