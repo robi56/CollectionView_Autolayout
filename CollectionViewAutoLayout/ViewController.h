8e124fc0e725f534ca6ee6fac0adaab57576780c
robi <nandi@ipvision-inc.com>
2015-05-17 11:11:49 +0600

    Modified ViewController.h

--- CollectionViewAutoLayout/ViewController.h
+++ CollectionViewAutoLayout/ViewController.h
@@ -19,8 +19,6 @@ typedef NS_ENUM(NSInteger, ImformationType){
 
 @property (nonatomic,strong) IBOutlet UICollectionView *picCollectionView;
 @property (nonatomic,strong) IBOutlet UISearchBar *picSearchBar;
-@property (nonatomic,strong) IBOutlet UISearchBar *picSearchBar1;
-@property (nonatomic,strong) IBOutlet UISearchBar *picSearchBar1;
 
 @end
 
